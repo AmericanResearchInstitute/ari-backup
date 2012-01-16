a252f37ccfddd5082771c456a81f5ad156256eac
Randy Barlow <rbarlow@lexile.com>
2012-01-16 17:52:21 -0500

    I messed everything up.

--- ari-backup/ari_backup.py
+++ ari-backup/ari_backup.py
@@ -34,65 +34,37 @@ class AriBackup:
     Arbitrary remote job execution is also provided.
 
     '''
-    def __init__(self, hostname):
-        # Gotta have a hostname...
-        self.hostName = hostname
+    def __init__(self, source_hostname, label, **kwargs):
+        # This is the host that has the source data.
+        self.source_hostname = source_hostname
+
+        # The name of the backup job (this will be the name of the directory in the backup store
+        # that has the data).
+        self.label = label
 
         # setup logging
-        self.logger = Logger('AriBackup (%s)' % hostname)
-
-        # TODO: Add validation on these paths
-        self.sshExec = '/usr/bin/ssh'
-        self.backupStore = '/srv/backup-store'
-        self.backupTmp = '%s/tmp' % ( self.backupStore )
-
-        # These will be set by the backup script
-        self.userName = ''
-        # these may be set if we're doing a snap shop backup
-        self.vmHostName = ''
-        self.lvList = []
-        # a list of tuples with the snapshot paths and where they should be
-        # mounted
-        self.lvSnapShots = []
-        self.snapShotMountPointBasePath = os.path.join('/tmp', self.hostName)
+        self.logger = Logger('AriBackup ({label})'.format(label=label))
 
         # Include nothing by default
-        self.includeDirList = []
-        self.includeFileList = []
-
+        self.include_dir_list = []
+        self.include_file_list = []
         # Exclude nothing by default
         # Add the '**' exclude to the end of the list later
-        self.excludeDirList = []
-        self.excludeFileList = []
+        self.exclude_dir_list = []
+        self.exclude_file_list = []
 
-        # Define argument list to be passed to the rdiff_backup module
-        # This will be in the format we'd normally pass to the command-line
-        # i.e. [ '--include', '/dir/to/include', '--exclude', '/dir/to/exclude']
-        self.argList = []
-
-		# setup some default rdiff-backup options
-        self.argList.append('--exclude-device-files')
-        self.argList.append('--exclude-fifos')
-        self.argList.append('--exclude-sockets')
-
-        # Bring the terminal verbosity down so that we only see errors
-        self.argList += ['--terminal-verbosity', '1']
-
-        # Default to no SSH compression
-        self.sshCompression = False
+        # Read the settings from YAML, and mash them with stuff from kwargs
+        self._process_settings(**kwargs)
 
         self.logger.info('initialized')
 
-    def __del__(self):
-        '''Our deconstructor, mostly for logging that things have stopped.
+    def run_job(self, command, host='localhost'):
+        try:
+            return self._run_job(command, host)
+        except Exception, e:
+            exit(str(e))
 
-        This is especially useful for knowing that the script has
-        abruptly stopped due to a syntax error or something similar.
-
-        '''
-        self.logger.info('stopped')
-
-    def runJob(self, command):
+    def _run_job(self, command, host='localhost'):
         '''Runs an arbitrary job on the remote host.
 
         Given an input string, we attempt to execute it on the remote
@@ -102,7 +74,11 @@ class AriBackup:
         # Spawn off an SSH child process to do the given command
         # Wait for ssh to end before processing
         try:
-            args = shlex.split('%s %s@%s %s' % (self.sshExec, self.userName, self.hostName, command.strip('"')))
+            if host == 'localhost':
+                args = shlex.split(command)
+            else:
+                args = shlex.split('%s %s@%s %s' % (self.sshExec,
+                    self.userName, self.hostName, command.strip('"')))
             self.logger.info('runJob %r' % args)
             p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = p.communicate()
@@ -115,147 +91,152 @@ class AriBackup:
         except IOError:
             exit(self.logger, 'Unable to execute %s. Exiting' % self.sshExec)
 
-        # TODO This is strange, comparing the value of an exit code to a string.  Perhaps this should be an int 0, not a string '0'?
-        if exitcode > '0':
-            self.logger.error('%s terminated with errors and likely requires intervention. The command attempted was "%s". Backups will attempt to proceed.' %
-                (self.sshExec, command)
-            )
+        if exitcode > 0:
+            error_message = ('[{host}] A command terminated with errors and likely requires intervention. The '
+                'command attempted was "{command}".').format(
+                    host=host, command=command)
+            raise Exception(error_message)
 
-        return exitcode
 
-    def runLocalJob(self, command):
-        '''Runs an arbitrary job on the local host.'''
+    def run_backup(self):
         try:
-            args = shlex.split(command)
-            self.logger.info('runLocalJob %r' % args)
-            p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate()
+            self._run_backup()
+        except Exception, e:
+            exit(str(e))
 
-            if stdout:
-                self.logger.info(stdout)
-            if stderr:
-                self.logger.error(stderr)
-            exitcode = p.returncode
 
-        except IOError:
-            exit(self.logger, 'Unable to execute %s. Exiting' % command)
-
-        # TODO This is strange, comparing the value of an exit code to a string.  Perhaps this should be an int 0, not a string '0'?
-        if exitcode > '0':
-            self.logger.error('%s terminated with errors and likely requires intervention. Backups will attempt to proceed.' %
-                command
-            )
-
-        return exitcode
-
-    def runJobOnVMHost(self, command):
-        '''
-        This takes a given input string and attempts to execute
-        it on the VM host via SSH
-        '''
-
-        # Set up a log prefix so we know what VM host is being used in
-        # output of communicate() handles
-        log_prefix = '(vmHost %s) ' % self.vmHostName
-
-        # Spawn off an SSH child process to do the given command
-        # Wait for ssh to end before processing
-        try:
-            args = shlex.split('%s %s@%s "%s"' % (self.sshExec, self.userName, self.vmHostName, command.strip('"')))
-            self.logger.info('runJobOnVMHost %r' % args)
-            p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate()
-
-            if stdout:
-                self.logger.info(log_prefix + stdout)
-            if stderr:
-                self.logger.error(log_prefix + stderr)
-            exitcode = p.returncode
-        except IOError:
-            exit(self.logger, 'Unable to execute %s. Exiting' % self.sshExec)
-
-        # TODO This is strange, comparing the value of an exit code to a string.  Perhaps this should be an int 0, not a string '0'?
-        if exitcode > '0':
-            self.logger.error('%s terminated with errors and likely requires intervention. The command attempted was "%s". Backups will attempt to proceed.' %
-                (self.sshExec, command)
-            )
-
-        return exitcode
-
-    def runBackup(self):
+    def _run_backup(self):
         '''Run typical rdiff-backup backup job.
 
         This generates an argument list as expected by rdiff_backup,
         and then passes it directly to the rdiff_backup module
 
         '''
-        self.logger.info('runBackup started')
+        self._run_pre_backup_hooks()
 
-        if not self.sshCompression:
-            self.argList.append('--ssh-no-compression')
+        self.logger.info('run_backup started')
+
+        # Define argument list to be passed to the rdiff_backup module
+        # This will be in the format we'd normally pass to the command-line
+        # i.e. [ '--include', '/dir/to/include', '--exclude', '/dir/to/exclude']
+        arg_list = []
+
+        # setup some default rdiff-backup options
+        arg_list.append('--exclude-device-files')
+        arg_list.append('--exclude-fifos')
+        arg_list.append('--exclude-sockets')
+
+        # Bring the terminal verbosity down so that we only see errors
+        arg_list += ['--terminal-verbosity', '1']
+
+        if not self.ssh_compression:
+            arg_list.append('--ssh-no-compression')
 
         # Populate self.argument list
-        for excludeDir in self.excludeDirList:
-            self.argList.append('--exclude')
-            self.argList.append(excludeDir)
+        for exclude_dir in self.exclude_dir_list:
+            arg_list.append('--exclude')
+            arg_list.append(exclude_dir)
 
-        for excludeFile in self.excludeFileList:
-            self.argList.append('--exclude-filelist')
-            self.argList.append(excludeFile)
+        for exclude_file in self.exclude_file_list:
+            arg_list.append('--exclude-filelist')
+            arg_list.append(exclude_file)
 
-        for includeDir in self.includeDirList:
-            self.argList.append('--include')
-            self.argList.append(includeDir.replace(' ', '\x20'))
+        for include_dir in self.include_dir_list:
+            arg_list.append('--include')
+            # TODO: Is this \x20 dealio important? If so, shouldn't it be on exclude_dir too?
+            arg_list.append(include_dir.replace(' ', '\x20'))
 
-        for includeFile in self.includeFileList:
-            self.argList.append('--include-filelist')
-            self.argList.append(includeFile)
+        for include_file in self.include_file_list:
+            arg_list.append('--include-filelist')
+            arg_list.append(include_file)
 
         # Exclude everything else
-        self.argList.append('--exclude')
-        self.argList.append('**')
-
-        # Configure a tmpdir on the backup volume
-        # TODO Custom Tempdir not supported until rdiff-backup 1.1.13
-        # self.argList.append('--tempdir')
-        # self.argList.append(self.backupTmp)
+        arg_list.append('--exclude')
+        arg_list.append('**')
 
         # Let rdiff know where it's aimed
-        self.argList.append('%s@%s::/' % (self.userName, self.hostName) )
-        self.argList.append('%s/%s' % ( self.backupStore, self.hostName) )
+        if self.source_hostname == 'localhost':
+            arg_list.append('/')
+        else:
+            arg_list.append('%s@%s::/' % (self.username, self.source_hostname) )
+        arg_list.append('%s/%s' % ( self.backup_store, self.label) )
 
+        rdiff_backup.Main.error_check_Main(arg_list)
+
+        self.logger.info('run_backup completed')
+
+        self._run_post_backup_hooks()
+
+
+    def remove_older_than(self, timespec):
+        '''Trims old backups older than timestamp
+
+        Uses rdiff-backup's --remove-old-than feature to trim old
+        increments from the backup history
+
+        '''
+        self.logger.info('remove_older_than %s started' % timespec)
+
+        arg_list = []
+        arg_list.append('--force')
+        arg_list.append('--remove-older-than')
+        arg_list.append(timespec)
+        arg_list.append('%s/%s' % (self.backup_store, self.label))
+
+        rdiff_backup.Main.error_check_Main(arg_list)
+
+        self.logger.info('remove_older_than %s completed' % timespec)
+
+
+    def _rdiff_backup(self, arg_list):
+        # TODO: Does this *really* start a new thread? Does that even matter for our code?
         # Call rdiff-backup, spawning a new thread and waiting for it
         # to execute before finishing
         try:
-            rdiff_backup.Main.error_check_Main(self.argList)
         except:
             exit(self.logger, 'An unforseen excpetion has arisen. Exiting.')
 
-        self.logger.info('runBackup completed')
 
-    def doMysqlDump(self, gzip=False, routines=True):
-        '''Performs MySQL script dump using mysqldump.
+    def _process_settings(self, **kwargs):
+        ###################
+        # These are in YAML
+        ###################
+        # TODO: Add validation on these paths
+        self.ssh_exec = '/usr/bin/ssh'
+        self.backup_store = '/srv/backup-store'
+        # These will be set by the backup script
+        self.remote_username = ''
+        # Default to no SSH compression
+        self.ssh_compression = False
+        ##################
 
-        Constructs a suitable mysqldump command-line and passes it to runJob
+        settings = yaml
+        for key, value in enumerate(kwargs):
+            settings[key] = value
+        for key, value in enumerate(settings):
+            setattr(self, key, value)
 
-        gzip is a bool, and will pipe through gzip to all.sql.gz
+
+    def __del__(self):
+        '''Our deconstructor, mostly for logging that things have stopped.
+
+        This is especially useful for knowing that the script has
+        abruptly stopped due to a syntax error or something similar.
 
         '''
-        dumpExec = "mysqldump -u backup --all-databases --force"
+        self.logger.info('stopped')
 
-        if routines:
-            dumpExec = '%s --routines' % dumpExec
 
-        # We shouldn't ever need to gzip, but you can if you want to.
-        if gzip:
-            gzPipe = "gzip --rsyncable --to-stdout"
-            outFile = "/var/backups/mysql/all.sql.gz"
-            command = "%s | %s > %s" % (dumpExec, gzPipe, outFile)
-        else:
-            outFile = "/var/backups/mysql/all.sql"
-            command = "%s > %s" % (dumpExec, outFile)
-
-        self.runJob(command)
+class LVMBackup(AriBackup):
+    def __init__(self, source_hostname, label, *args, **kwargs):
+        super(self.__class__, self).__init__(source_hostname, label, *args, **kwargs)
+        # This is a list of 2-tuples, where each inner 2-tuple expresses the lv to back up, and
+        # the mount point for that LV. For example, (('hostname/root, '/'),)
+        self.lv_list = []
+        # a list of tuples with the snapshot paths and where they should be
+        # mounted
+        self.lv_snap_shots = []
+        self.snap_shot_mount_point_base_path = os.path.join('/tmp', self.label)
 
     def _createSnapShots(self):
         '''Creates snapshots of all the volumns listed in self.lvList.'''
@@ -357,30 +338,3 @@ class AriBackup:
         self._deleteSnapShots()
 
         self.logger.info('runSnapShotBackup completed')
-
-    def removeOlderThan(self, timespec):
-        '''Trims old backups older than timestamp
-
-        Uses rdiff-backup's --remove-old-than feature to trim old
-        increments from the backup history
-
-        '''
-        self.logger.info('removeOlderThan %s started' % timespec)
-        # use a local argList since we're doing something entirely different
-        # than a backup
-        argList = []
-        argList.append('--force')
-        argList.append('--remove-older-than')
-        argList.append(timespec)
-        argList.append('%s/%s' % (self.backupStore, self.hostName))
-
-        # Call rdiff-backup, spawning a new thread and waiting for it
-        # to execute before finishing
-        try:
-            import rdiff_backup.Main
-            rdiff_backup.Main.error_check_Main(argList)
-            self.logger.info('removeOlderThan %s completed' % timespec)
-        except IOError:
-            exit(self.logger, 'Unable to execute %s. Exiting...' % self.backupExec)
-        except:
-            exit(self.logger, 'An unforseen exception has arisen. Exiting...')
